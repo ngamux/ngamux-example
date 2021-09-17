ada6dfbcd2ed1f8d2901b721bce4c457d996db4a
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-09-17 09:17:07 +0700

    add middleware example

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ngamux/ngamux-example
 
 go 1.17
 
-require github.com/ngamux/ngamux v0.0.0-20210916001513-162dc6e29fdb
+require github.com/ngamux/ngamux v0.0.0-20210917020142-988b7bec369d
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -7,23 +7,37 @@ import (
 	"github.com/ngamux/ngamux"
 )
 
+func globalMiddleware(next ngamux.HandlerFunc) ngamux.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) error {
+		fmt.Println("hello from global middleware")
+		return next(rw, r)
+	}
+}
+
+func routeMiddleware(next ngamux.HandlerFunc) ngamux.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) error {
+		fmt.Println("hello from route middleware")
+		return next(rw, r)
+	}
+}
+
 func main() {
 	mux := ngamux.NewNgamux(ngamux.Config{
 		RemoveTrailingSlash: true,
 	})
+	mux.Use(globalMiddleware)
 
-	mux.Use(func(next ngamux.HandlerFunc) ngamux.HandlerFunc {
-		return func(rw http.ResponseWriter, r *http.Request) error {
-			fmt.Println("hello from middleware")
-			return next(rw, r)
-		}
-	})
-
-	mux.Get("/", func(rw http.ResponseWriter, r *http.Request) error {
-		fmt.Fprintln(rw, "hello from users handler")
-		fmt.Println("hello from handler")
+	mux.Get("/", routeMiddleware(func(rw http.ResponseWriter, r *http.Request) error {
+		fmt.Fprintln(rw, "hello from / handler")
+		fmt.Println("hello / handler")
 		return nil
-	})
+	}))
+
+	mux.Get("/users", ngamux.WithMiddlewares(routeMiddleware)(func(rw http.ResponseWriter, r *http.Request) error {
+		fmt.Fprintln(rw, "hello from /users handler")
+		fmt.Println("hello from /users handler")
+		return nil
+	}))
 
 	http.ListenAndServe(":8080", mux)
 }
